f321798952004748d8c8865ac0ad7b1cf49c8c2e
samu32ELTE <33515984+samu32ELTE@users.noreply.github.com>
2023-03-08 12:07:31 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from my_first_module import __version__
+
+setup(
+    name='my_first_module',
+    version=__version__,
+
+    url='https://github.com/samu32ELTE/my_first_module',
+    author='Sámuel G. Balogh',
+    author_email='samu32@caesar.elte.hu',
+
+    py_modules=['my_first_module'],
+)
