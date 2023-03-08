fd74fa31c824691383d971b9b364db6d183635cd
samu32ELTE <33515984+samu32ELTE@users.noreply.github.com>
2023-03-08 15:58:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
+import sys
 from setuptools import setup
 from myfirstmodule import __version__
 
+
+if sys.version_info[:2] < (3, 8):
+    error = (
+        "Myfirstmodule 2.7+ requires Python 3.8 or later (%d.%d detected). \n"
+        "For Python 2.7, please install version 2.2 using: \n"
+    )
+    sys.stderr.write(error + "\n")
+    sys.exit(1)
+
+with open("myfirstmodule/__init__.py") as fid:
+    for line in fid:
+        if line.startswith("__version__"):
+            version = line.strip().split()[-1][1:-1]
+            break
+
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
-setup(
-    name='myfirstmodule',
-    version=__version__,
-    url='https://github.com/samu32ELTE/myfirstmodule/',
-    author='Sámuel G. Balogh',
-    author_email='samu32@caesar.elte.hu',
-    py_modules=['myfirstmodule'],
-    install_requires= REQUIREMENTS
-)
+
+if __name__ == "__main__":
+    setup(
+        name='myfirstmodule',
+        version=__version__,
+        url='https://github.com/samu32ELTE/myfirstmodule/',
+        author='Sámuel G. Balogh',
+        author_email='samu32@caesar.elte.hu',
+        py_modules=['myfirstmodule'],
+        install_requires= REQUIREMENTS
+    )
 
 
