48b163da62e8db195829940b237e167885bda10e
samu32ELTE <102521546+BaloghHal@users.noreply.github.com>
2023-03-08 16:48:47 +0100

    Update myfirstmodule.py

--- myfirstmodule.py
+++ myfirstmodule.py
@@ -2,15 +2,18 @@ __version__ = '1.0.0'
 
 import numpy
 import networkx
-import community
-
+from community import best_partition as Louvain
 print('Numpy succesfully loaded.')
 
 def print_sum(a, b, *args):
     if args is None:
         print ('sum:',a+b)
+        G = nx.barabasi_albert_graph(10,2)
+        print (G.nodes(), Louvain(G))
     else:
         x = a + b
         for arg in args:
             x+= arg
         print ('sum:',x)
+        G = nx.barabasi_albert_graph(10,2)
+        print (G.nodes(), Louvain(G))
